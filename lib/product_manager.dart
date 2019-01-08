8204214ae129110cbcad7bb75485d3d8c4e754f3
xtzaferis <xtzaferis@gmail.com>
2019-01-08 23:11:50 +0100

    first commit

---
+++ lib/product_manager.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+import './product_control.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+  ProductManager({this.startingProduct});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.startingProduct != null) {
+      _products.add(widget.startingProduct);
+    } 
+  }
+
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: ProductControl(_addProduct),
+        ),
+        Expanded(
+          child: Products(_products)
+        )
+      ],
+    );
+  }
+}
---
+++ lib/products.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Products extends StatelessWidget {
+  final List<String> products;
+
+  Products([this.products = const []]);
+
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductList() {
+    Widget productCard =
+        Center(child: Text('No products found, please add some'));
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    }
+    return productCard;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildProductList();
+  }
+}
